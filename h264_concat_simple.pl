e2135c3bf240c1b883884522a7edca9951bfec40
h-nimrod <h.tanabe@gmail.com>
2014-10-14 20:50:38 +0900

    remove unnecessary lines

--- h264_concat_simple.pl
+++ h264_concat_simple.pl
@@ -7,7 +7,6 @@ use Cwd;
 use File::Temp qw/tempfile/;
 use Data::Dumper;
 
-
 my $size = qq/960x540/;
 my $bitrate = qq/1500000/;
 #my $ffmpeg = "/usr/bin/ffmpeg";
@@ -17,15 +16,11 @@ my $basedir = Cwd::getcwd() || "";
 my $file_pattern = q{\.(h264)$};
 my @video_files;
 
-
-
-
 ##############################
 sub search {
     my $file = $File::Find::name;
     if ($file =~ /${file_pattern}/i) {
 		$file =~ s/^\.//;
-		
 		$file = $basedir . $file;
 		push @video_files, $file;
     }
@@ -34,7 +29,6 @@ sub search {
 ##############################
 sub dump_filelist {
 	my $wh = shift || return;
-
 	foreach (sort @video_files) {
 		my $str = "file '" . $_ . "'\n";
 		print $wh $str;
@@ -48,14 +42,12 @@ sub main {
 	my ($wh, $fname) = tempfile;
 	dump_filelist $wh;
 	close $wh;
-	
+
 	if (!-f $ffmpeg) {
 		print `$ffmpeg -f concat  -i $fname -s $size -b:v $bitrate merge.wmv`;
 	}
-
 	unlink $fname;
 }
-
 
 ##############################
 main;
